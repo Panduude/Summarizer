c17d2691a643148f4a37febb9fe5322eb4801615
Pandu J. Agyle <Justikapandu@gmail.com>
2025-06-01 19:38:26 +0700

    syalalallala

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ def load_model():
 summarizer = load_model()
 
 st.set_page_config(page_title="Text Summarizer", page_icon="📝")
-st.title("📝 Text Summarizer")
 
 text = st.text_area("Enter text to summarize", height=200)
 max_length = st.slider("Max summary length", 30, 300, 130)
